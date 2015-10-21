9db7eb60ebae51fad4965e837b8b2c18f9754920
SHIMANE, Yoshikazu <shima@timedia.co.jp>
2015-10-21 15:22:44 +0900

    Initial commit.

---
+++ src/main/java/com/example/springsample/controller/HomeControler.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.springsample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Yoshikazu
+ */
+@Controller
+public class HomeControler {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome(Model model) {
+        model.addAttribute("greeting", "Welcome to Web Store!");
+        model.addAttribute("tagline", "The one and only amazing webstore");
+
+        return "welcome";
+    }
+}
---
+++ src/main/java/com/exapmle/springsample/domain/DomainBase.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.exapmle.springsample.domain;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ *
+ * @author Yoshikazu
+ */
+public class DomainBase {
+
+    @Override
+    public int hashCode(){
+        return HashCodeBuilder
+                .reflectionHashCode(17, 37, this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+    
+}
